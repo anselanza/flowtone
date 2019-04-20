0fac120919a9bf767da15b24d81b28c4c5176832
Stephen Buchanan <stephen.buchanan@random.studio>
2019-04-20 18:44:41 +0200

    adding Tone nodes on component did mount for each widget

--- src/Toybox.js
+++ src/Toybox.js
@@ -1,9 +1,46 @@
 import React, { Component } from 'react';
 import Tone from 'tone';
 
-let network = {};
+let nodes = [];
 
 class Toybox extends Component {
+
+  componentDidMount() {
+    this.props.widgets.forEach(widget => {
+
+      let node;
+
+      switch (widget.type) {
+        case 'Tone.Master':
+        // created on initialisation by ToneJS
+        console.log('master; ignore');
+      break;
+
+      case 'Tone.Oscillator':
+          console.log('create oscillator');
+          node = new Tone.Oscillator();
+      break;
+
+      case 'Tone.Noise':
+        console.log('create noise');
+        node = new Tone.Noise();
+      break;
+
+      case 'Tone.Filter':
+        console.log('create filter');
+        node = new Tone.Filter();
+      break;
+
+      default:
+          console.error('unknown widget.type for:', widget);
+        }
+
+      console.log('adding node', node);
+      nodes.push(node);
+      
+    });
+  }
+
   render() {
     return (
       <div className="Toybox">
