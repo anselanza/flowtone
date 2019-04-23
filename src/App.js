c35e74d97997c11bc0976b8bf3d2bc513fde92c9
Stephen Buchanan <stephen.buchanan@random.studio>
2019-04-23 10:55:12 +0200

    rename Toybox to Board

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux'
 
 import store from './redux/store'
 import './App.css';
-import Board from './components/Toybox';
+import Board from './components/Board';
 
 
 class App extends Component {
