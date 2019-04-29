d44f0d9e18cf69513e6c57437771a2d8f9bfee5d
Stephen Buchanan <stephen.buchanan@random.studio>
2019-04-29 12:43:45 +0200

    basic widget search working in toybox

--- src/components/Toybox.js
+++ src/components/Toybox.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 import { Container, Form, Row } from 'react-bootstrap';
 
+
 class Toybox extends Component {
+
+  state = {
+    searchString: ''
+  }
+
+  handleSearch = (event) => {
+    this.setState({ searchString: event.target.value });
+  }
+
+  widgetList = () => this.state.searchString === ''
+    ? this.props.widgets
+    : this.props.widgets.filter(w => w.name.toLowerCase().includes(this.state.searchString.toLowerCase()));
+
   render = () => (
     <div className="toybox">
 
       <Container>
         <Form>
             <Form.Group as={Row} controlId="searchToybox">
-              <Form.Control size="sm" type="text" placeholder="search" />
+              <Form.Control 
+                size="sm" 
+                type="text" 
+                placeholder="search" 
+                value={this.state.searchString} 
+                onChange={this.handleSearch}
+              />
             </Form.Group>
     
-            {this.props.widgets && this.props.widgets.map(widget => (
+            {this.widgetList().map(widget => (
               <div key={widget.id}>
                 <span>{widget.name}</span> 
               </div>
