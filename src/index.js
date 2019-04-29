7ef671c5b852ac478d7c4004aede17d8572b341d
Stephen Buchanan <stephen.buchanan@random.studio>
2019-04-29 12:24:05 +0200

    bootswatch slate theme

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+import "bootswatch/dist/slate/bootstrap.css";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
