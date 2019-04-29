b2efbb1d334b9f7a1071426ec2bf8e926dfddecd
Stephen Buchanan <stephen.buchanan@random.studio>
2019-04-29 12:32:30 +0200

    lay out toybox with bootstrap form

--- src/components/Toybox.js
+++ src/components/Toybox.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Form, Row } from 'react-bootstrap';
 
 class Toybox extends Component {
   render = () => (
-    <Container className="toybox">
-      <h2>Toybox</h2>
-      {this.props.widgets && this.props.widgets.map(widget => (
-        <div key={widget.id}>
-          <span>{widget.name}</span> 
-        </div>
-      ))}
-    </Container>
+    <div className="toybox">
+
+      <Container>
+        <Form>
+            <Form.Group as={Row} controlId="searchToybox">
+              <Form.Control size="sm" type="text" placeholder="search" />
+            </Form.Group>
+    
+            {this.props.widgets && this.props.widgets.map(widget => (
+              <div key={widget.id}>
+                <span>{widget.name}</span> 
+              </div>
+            ))}
+    
+            </Form>
+      </Container>
+
+    </div>
   )
 }
 
